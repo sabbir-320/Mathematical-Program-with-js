3c0acbc9e818ca0e82320cf11282747a9309ef87
sabbir-320 <sabbir195661@gmail.com>
2020-07-16 16:45:43 +0600

    first push

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// // Inch to  feet convert
+// function inchToFeet(n) {
+//     var result = n / 12;
+//     return result
+// }
+// console.log(inchToFeet(120))
+// console.log(inchToFeet(61))
+
+// // find a leap year.. solve one
+// var IsLeapYear = 2024;
+// if (IsLeapYear % 4 == 0 && IsLeapYear % 100 != 0) {
+//     console.log("This year is leap year")
+// }
+// else if (IsLeapYear % 400 == 0) {
+//     console.log("This year is leap year")
+// }
+// else {
+//     console.log("This year is not leap year")
+// }
+// // .. solve tow
+// function isLeapYear(year) {
+//     return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+// }
+// console.log(isLeapYear('2000'))
+
+// Calculate Factorial of a number using for loop
+
+const 
+
+
+
